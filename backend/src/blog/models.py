365323c3e266ed91d02624f617f89e5258862114
Gaur <www.prashatgaur@gmail.com>
2020-07-17 12:52:07 -0900

    added models and admin view , migration is also run

--- backend/src/blog/models.py
+++ backend/src/blog/models.py
@@ -1,3 +1,59 @@
+"""
+"""
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
+    body = models.TextField(null=True)
+
+    slug = models.SlugField(max_length=255)
+
+    added_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    total_likes = models.BigIntegerField(default=0)
+    total_comments = models.BigIntegerField(default=0)
+
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        """
+        """
+        app_label = 'blog'
+
+
+class PostLike(models.Model):
+    """
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        """
+        """
+        app_label = 'blog'
+
+
+class PostComment(models.Model):
+    """
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    text = models.TextField()
+
+    added_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        """
+        """
+        app_label = 'blog'
